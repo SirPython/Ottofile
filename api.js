7318c2eb98700490ae50428578b09710c4b52e27
SirPython <SirPython@noreply.github.com>
2018-09-16 18:02:00 -0400

    cleaning up stuff

--- api.js
+++ api.js
@@ -4,9 +4,18 @@ class API {
     }
 }
 
-const API = [
-    {
-
-    },
-
+const routers = [
+    "https://cors-anywhere.herokuapp.com/",
+    "http://alloworigin.com/get?url=",
+    "http://www.whateverorigin.org/get?url=",
+    "http://anyorigin.com/go?url="
 ];
+
+/**
+ * A wrapper function for fetch that works with CORS.
+ */
+const fetchCORS = (url, options = {}, routerI = 0) =>
+    fetch(routers[routerI] + url, options)
+    .catch(() =>
+        ++routerI < routers.length ? fetchCORS(url, options, routerI) : null
+    );
--- lib.js
+++ lib.js
@@ -1,21 +1,5 @@
 const API_ROOT = "https://api.myjson.com/bins";
 
-const routers = [
-    "https://cors-anywhere.herokuapp.com/",
-    "http://alloworigin.com/get?url=",
-    "http://www.whateverorigin.org/get?url=",
-    "http://anyorigin.com/go?url="
-];
-
-/**
- * A wrapper function for fetch that works with CORS.
- */
-const fetchCORS = (url, options = {}, routerI = 0) =>
-    fetch(routers[routerI] + url, options)
-    .catch(() =>
-        ++routerI < routers.length ? fetchCORS(url, options, routerI) : null
-    );
-
 /**
  * Loads a myjson document. Each line in the myjson document should be a
  * different link to an article.
