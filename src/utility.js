a7486d7856ff990acfaee18bb12841f3f816f592
SirPython <SirPython@noreply.github.com>
2018-10-02 22:10:16 -0400

    utility executables need to be here

--- src/utility.js
+++ src/utility.js
@@ -1,1 +1,14 @@
-console.log("test");
+const readline = require("readline");
+const fs       = require("fs");
+
+console.log("Loading files...");
+
+const files = {};
+
+
+readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}).question("Enter phrase to search for: ", (answer) => {
+    console.log(fs.readdirSync(""))
+});
